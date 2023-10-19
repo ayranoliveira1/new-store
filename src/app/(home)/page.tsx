7bb882662aceec532b7c1d925c24f1d004859dd3
ayranoliveira1 <ayranoliveira1@gmail.com>
2023-10-19 19:31:59 -0300

    fea

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Categories from "./components/categories";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image
+        src="/banner-home-01.png"
+        height={0}
+        width={0}
+        alt="Até 55% de desconto esse mês"
+        sizes="100vw"
+        className="h-auto w-full"
+      />
+
+      <div className="mt-8">
+        <Categories />
+      </div>
+    </div>
+  );
+}
