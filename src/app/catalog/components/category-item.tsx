8d7e095aa8fee82ba118e4b5f4c2173eb5318973
ayranoliveira1 <ayranoliveira1@gmail.com>
2023-10-21 17:08:01 -0300

    feat: add CategoryItem components

---
+++ src/app/catalog/components/category-item.tsx
@@ -0,0 +1,32 @@
+import { Category } from "@prisma/client";
+import Image from "next/image";
+
+interface CategotyItemProps {
+  category: Category;
+}
+
+const CategoryItem = ({ category }: CategotyItemProps) => {
+  return (
+    <div className="flex flex-col">
+      <div className="from-[#5033C3]-# flex h-[150px0] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-gradient-to-r to-[#4c23ed69]">
+        <Image
+          src={category.imageUrl}
+          alt={category.name}
+          width={0}
+          height={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%]"
+          style={{
+            objectFit: "contain",
+          }}
+        />
+      </div>
+
+      <div className="rounded-bl-lg rounded-br-lg bg-accent py-3">
+        <p className="text-center text-sm font-semibold">{category.name}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
