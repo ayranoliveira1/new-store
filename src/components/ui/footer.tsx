982fa914398e647f5145158a9347fb1cc247bf1b
ayranoliveira1 <ayranoliveira1@gmail.com>
2023-10-21 15:36:33 -0300

    feat: add footer components

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4 text-[0.625rem]">
+      © 2023 Copyright <span className="font-semibold">New Store</span>
+    </div>
+  );
+};
+
+export default Footer;
